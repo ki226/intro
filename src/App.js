1e6c8e741056ad0ab8b51b31c59b6e047ad58d90
Mac <tkddn088@gmail.com>
2020-09-02 18:40:40 +0900

    수정1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React, { Component } from "react";
 import Typist from "react-typist";
 import "./App.scss";
 import "./Typist.scss";
-import IntroducePdf from "./Front-end 이승민.pdf";
 import introduceImg from "./introduceimg.png";
+import selfie from "./selfie.jpeg";
 
 class Introduce extends React.Component {
   render() {
     return (
       <div className="Introduce">
-        <Typist>
-          <span>
-            안 녕 하 세 요.
-            <br />
-            <Typist.Delay ms={1000} />
-            f r o n t - e n d 신 입 개 발 자 <br />
-            <Typist.Delay ms={1000} />이 승 민 입 니 다.
-          </span>
-        </Typist>
-        <img className="image" src={introduceImg} />
+        <div className="head">
+          <img className="selfie" src={selfie}></img>
+
+          <Typist className="typingtext">
+            <span>
+              안 녕 하 세 요.
+              <br />
+              <Typist.Delay ms={1000} />
+              F r o n t - e n d 신 입 개 발 자 <br />
+              <Typist.Delay ms={1000} />이 승 민 입 니 다.
+            </span>
+          </Typist>
+        </div>
+        <img className="resume" src={introduceImg} />
       </div>
     );
   }
