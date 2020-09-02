55cc9b3c8396af58bf3a092095f53d9580874c00
Mac <tkddn088@gmail.com>
2020-09-02 19:44:01 +0900

    수정2

---
+++ src/Pages/Main.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Typist from "react-typist";
+import "../Styles/Main.scss";
+import introduceImg from "../Images/introduceimg.png";
+import selfie from "../Images/selfie.jpeg";
+
+function Main() {
+  return (
+    <div className="Introduce">
+      <div className="head">
+        <Typist className="typingtext">
+          <span>
+            안 녕 하 세 요.
+            <br />
+            <Typist.Delay ms={1000} />
+            F r o n t - e n d 신 입 개 발 자 <br />
+            <Typist.Delay ms={1000} />이 승 민 입 니 다.
+          </span>
+        </Typist>
+        <img className="selfie" src={selfie}></img>
+      </div>
+      <img className="resume" src={introduceImg} />
+    </div>
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import Routes from "./Routes";
+import "./Styles/reset.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <Routes />
+  </>,
   document.getElementById("root")
 );
